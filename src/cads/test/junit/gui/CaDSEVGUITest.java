b0de0bfe65e90c88ec12c0773c0ce93f13b0e612
scimbe <scimbe@gmail.com>
2015-12-30 15:15:14 +0100

    add tester for GUI

---
+++ src/cads/test/junit/gui/CaDSEVGUITest.java
@@ -0,0 +1,62 @@
+package cads.test.junit.gui;
+
+import static org.junit.Assert.*;
+
+import org.cads.ev3.gui.CaDSGripperGUIFX;
+import org.cads.ev3.gui.ICaDSGUIUpdater;
+import org.cads.ev3.rmi.ICADSRMIComsumer;
+import org.junit.Test;
+
+import cadSRMIInterface.IIDLCaDSEV3RMIMoveGrapper;
+import cadSRMIInterface.IIDLCaDSEV3RMIMoveHorizontally;
+import cadSRMIInterface.IIDLCaDSEV3RMIMoveVertical;
+
+public class CaDSEVGUITest implements IIDLCaDSEV3RMIMoveGrapper, IIDLCaDSEV3RMIMoveHorizontally,
+IIDLCaDSEV3RMIMoveVertical, ICADSRMIComsumer {
+
+	@Test
+	public void test() {
+		CaDSGripperGUIFX.startGUI(this, this, this, this);
+		//fail("Not yet implemented");
+	}
+
+	@Override
+	public void register(ICaDSGUIUpdater arg0) {
+		System.out.println("New Observer");
+	}
+
+	@Override
+	public void update(String arg0) {
+		// called if Combobox is updated 
+		System.out.println("Combo Box updated " + arg0);
+	}
+
+	@Override
+	public void moveVerticalToDegree(int arg0, int arg1) throws Exception {
+		System.out.println("Call to move vertical -  TID: " + arg0 +  " degree " + arg1);
+		
+	}
+
+	@Override
+	public void moveMoveHorizontallyToDegree(int arg0, int arg1) throws Exception {
+		System.out.println("Call to move horizontally -  TID: " + arg0 +  " degree " + arg1);
+		
+	}
+
+	@Override
+	public void stop(int arg0) throws Exception {
+		System.out.println("Stop movement....");
+	}
+
+	@Override
+	public void grap(int arg0) throws Exception {
+		System.out.println("Grap....");
+		
+	}
+
+	@Override
+	public void release(int arg0) throws Exception {
+		System.out.println("Release....");
+	}
+
+}
